a9ab08f937f83f503a9ce3c2a7a59490dfd0a14a
ssenchh1 <oleksandr.slinkin@nure.ua>
2021-05-18 12:54:32 +0300

    Fixed issues

---
+++ Homework3/CSVFormatter/CSVFormatter/Formats/CsvFormat.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CSVFormatter
+{
+    class CsvFormat : IFormat
+    {
+        private char separator;
+
+        public void GetNeededInfo()
+        {
+            Console.Write("Enter separator: ");
+            separator = Console.ReadKey().KeyChar;
+        }
+
+        public string Execute<T>(IEnumerable<T> list, List<string> fields)
+        {
+            //Adding fields that we will write
+            var properties = GetRequiredProperties<T>(typeof(T), fields);
+
+            //writing a new line for each element of collection
+            var sb = new StringBuilder();
+
+            foreach (var prop in properties)
+            {
+                sb.Append(prop.Name + separator);
+            }
+
+            sb.Append("\r\n");
+
+            foreach (var person in list)
+            {
+                var str = string.Empty;
+
+                foreach (var field in properties)
+                {
+                    str += field.GetValue(person);
+                    str += separator;
+                }
+                
+                sb.AppendLine(str);
+            }
+
+            return sb.ToString();
+        }
+
+        public IEnumerable<PropertyInfo> GetRequiredProperties<T>(Type type, List<string> fields)
+        {
+            var properties = new List<PropertyInfo>();
+
+            foreach (var propertyInfo in type.GetProperties())
+            {
+                foreach (var field in fields)
+                {
+                    if (string.Equals(field.ToLower().Trim(), propertyInfo.Name.ToLower()))
+                    {
+                        properties.Add(propertyInfo);
+                    }
+                }
+            }
+
+            return properties;
+        }
+    }
+}
--- Homework3/CSVFormatter/CSVFormatter/Program.cs
+++ Homework3/CSVFormatter/CSVFormatter/Program.cs
@@ -19,7 +19,7 @@ namespace CSVFormatter
 
             string pathToFile = @$"C:\Users\{Environment.UserName}\Desktop\persons.csv";
 
-            var formatter = new Formatter(new SvgFormat());
+            var formatter = new Formatter(new CsvFormat());
             formatter.Run(persons, pathToFile);
         }
     }
