05bc09a6e35ba0fb301d7c6c17a23f084c33c1f9
ssenchh1 <oleksandr.slinkin@nure.ua>
2021-05-18 13:34:14 +0300

    Fixed issues

--- Homework3/CSVFormatter/CSVFormatter/Program.cs
+++ Homework3/CSVFormatter/CSVFormatter/Program.cs
@@ -19,7 +19,7 @@ namespace CSVFormatter
 
             string pathToFile = @$"C:\Users\{Environment.UserName}\Desktop\persons.csv";
 
-            var formatter = new Formatter(new CsvFormat());
+            var formatter = new Formatter();
             formatter.Run(persons, pathToFile);
         }
     }
