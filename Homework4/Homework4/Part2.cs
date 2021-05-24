a37f0a2baf4fea6be4cd446204242cf7ccf5b320
ssenchh1 <oleksandr.slinkin@nure.ua>
2021-05-24 13:35:57 +0300

    fixed formatting

--- Homework4/Homework4/Part2.cs
+++ Homework4/Homework4/Part2.cs
@@ -8,12 +8,16 @@ namespace Homework4
     {
         public static void First(List<object> list)
         {
-            list.OfType<Film>().SelectMany(f => f.Actors).ToList().ForEach(i => Console.WriteLine(i.Name));
+            list.OfType<Film>()
+                .SelectMany(f => f.Actors).ToList()
+                .ForEach(i => Console.WriteLine(i.Name));
         }
 
         public static void Second(List<object> list)
         {
-            list.OfType<Film>().SelectMany(f => f.Actors.Where(a => a.Birthdate.Month == 8)).ToList().ForEach(i => Console.WriteLine(i.Name));
+            list.OfType<Film>()
+                .SelectMany(f => f.Actors.Where(a => a.Birthdate.Month == 8)).ToList()
+                .ForEach(i => Console.WriteLine(i.Name));
         }
 
         public static void Third(List<object> list)
@@ -25,13 +29,15 @@ namespace Homework4
 
         public static void Fourth(List<object> list)
         {
-            list.OfType<Article>().GroupBy(a => a.Author).ToList()
+            list.OfType<Article>()
+                .GroupBy(a => a.Author).ToList()
                 .ForEach(g => Console.WriteLine(g.Key + ": " + g.Count()));
         }
 
         public static void Fifth(List<object> list)
         {
-            list.OfType<ArtObject>().GroupBy(i => i.Author).ToList()
+            list.OfType<ArtObject>()
+                .GroupBy(i => i.Author).ToList()
                 .ForEach(g => Console.WriteLine(g.Key + ": " + g.Count()));
         }
 
@@ -41,8 +47,9 @@ namespace Homework4
 
             list.OfType<Film>().SelectMany(f => f.Actors)
                 .SelectMany(s => s.Name.Replace(" ", string.Empty).ToLower())
-                .Distinct().Select(a => 
-                {   
+                .Distinct()
+                .Select(a =>
+                {
                     i++;
                     return a;
                 }).ToList()
@@ -53,43 +60,51 @@ namespace Homework4
 
         public static void Seven(List<object> list)
         {
-            list.OfType<Article>().OrderBy(a => a.Author).ThenBy(a => a.Pages).ToList().
-                ForEach(a => Console.WriteLine(a.Name));
+            list.OfType<Article>()
+                .OrderBy(a => a.Author)
+                .ThenBy(a => a.Pages).ToList()
+                .ForEach(a => Console.WriteLine(a.Name));
         }
 
         public static void Eight(List<object> list)
         {
             list.OfType<Film>()
                 .SelectMany(f => f.Actors,
-                    (f, a) => new {Film = f, Actor = a})
-                .GroupBy(i => i.Actor).ToList().
-                ForEach(g => g.ToList()
+                    (f, a) => new { Film = f, Actor = a })
+                .GroupBy(i => i.Actor).ToList()
+                .ForEach(g => g.ToList()
                     .ForEach(a => Console.WriteLine(g.Key.Name + ": " + a.Film.Name)));
         }
 
         public static void Nine(List<object> list)
         {
             var a = list.OfType<Article>()
-                .Select(i => i.GetType().GetProperties().Where(p => p.PropertyType == typeof(int))
+                .Select(i => i.GetType()
+                    .GetProperties()
+                    .Where(p => p.PropertyType == typeof(int))
                     .Select(p => (int)p.GetValue(i)))
-                .Select(i => i.ToList()).
-                Union(list.OfType<Film>()
-                    .Select(i => i.GetType().GetProperties().Where(p => p.PropertyType == typeof(int))
+                .Select(i => i.ToList())
+                .Union(list.OfType<Film>()
+                    .Select(i => i.GetType()
+                        .GetProperties()
+                        .Where(p => p.PropertyType == typeof(int))
                         .Select(p => (int)p.GetValue(i)))
                     .Select(i => i.ToList()))
-                .Union(list.OfType<List<int>>().Select(i => i))
-                .Select(a => a.Sum()).Sum();
+                .Union(list.OfType<List<int>>()
+                    .Select(i => i))
+                .Select(a => a.Sum())
+                .Sum();
 
             Console.WriteLine();
         }
 
         public static void Ten(List<object> list)
         {
-            list
-                .OfType<Article>()
+            list.OfType<Article>()
                 .GroupBy(i => i.Author)
                 .ToList()
-                .ForEach(g => g.ToList().ForEach(a => Console.WriteLine(g.Key + ": " + a.Name[..20])));
+                .ForEach(g => g.ToList()
+                    .ForEach(a => Console.WriteLine(g.Key + ": " + a.Name[..20])));
             Console.WriteLine();
         }
     }
