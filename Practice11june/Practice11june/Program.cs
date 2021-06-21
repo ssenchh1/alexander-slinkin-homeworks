c1ee71a9ac22566eeda84c886625dc11838d8d79
ssenchh1 <oleksandr.slinkin@nure.ua>
2021-06-21 21:54:45 +0300

    little fix

--- Practice11june/Practice11june/Program.cs
+++ Practice11june/Practice11june/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using Practice11june.Domain.Model;
-using Practice11june.Infrastructure.Repository;
-using Practice11june.BLL.Services;
+﻿using Practice11june.BLL.Services;
 
 namespace Practice11june
 {
@@ -9,14 +6,6 @@ namespace Practice11june
     {
         static void Main(string[] args)
         {
-            DapperRepository<User> userRepo = new DapperRepository<User>();
-            DapperRepository<Country> countryRepo = new DapperRepository<Country>();
-
-            var user = new User {FullName = "Alex", Gender = "M", Email = "a", DateOfBirth = DateTime.Now, CreatedAt = DateTime.Now, Country_Code = 1};
-
-            var country = new Country {Country_Code = 1, Name = "USA"};
-            
-
             var hidingService = new HidingDataService();
 
             hidingService.HideInfo("a");
